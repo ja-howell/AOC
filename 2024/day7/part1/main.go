dfe94da227fce892c1e565c083292cbc1088ea5c
Jami Howell <jami.howell0@gmail.com>
2024-12-09 15:32:15 -0600

    Adds solution for day7 part1

---
+++ 2024/day7/part1/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data := getInput("input.txt")
+
+	equations := processData(data)
+
+	validEquations := getValidEquations(equations)
+
+	sum := sumEquations(validEquations)
+
+	fmt.Println(sum)
+
+}
+
+func getInput(file string) []string {
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	data := []string{}
+
+	for s.Scan() {
+		data = append(data, s.Text())
+	}
+
+	return data
+}
+
+func processData(data []string) map[int][]int {
+	equations := map[int][]int{}
+	for _, datum := range data {
+		b, a, f := strings.Cut(datum, ":")
+		if !f {
+			fmt.Print("Invalid Line: ")
+			fmt.Println(datum)
+		}
+
+		key, err := strconv.Atoi(b)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fields := strings.Fields(a)
+		nums := []int{}
+		for _, val := range fields {
+			num, err := strconv.Atoi(val)
+			if err != nil {
+				fmt.Print("Invalid Line: ")
+				fmt.Println(val)
+				log.Fatal(err)
+			}
+			nums = append(nums, num)
+		}
+		equations[key] = nums
+
+	}
+
+	return equations
+}
+
+func getValidEquations(equations map[int][]int) []int {
+	validTotals := []int{}
+
+	for total, nums := range equations {
+		if isValidEquation(total, nums) {
+			validTotals = append(validTotals, total)
+		}
+	}
+	return validTotals
+}
+
+func isValidEquation(target int, nums []int) bool {
+	if len(nums) == 1 {
+		return nums[0] == target
+	}
+
+	l := len(nums) - 1
+	if isValidEquation(target-nums[l], nums[:l]) {
+		return true
+	}
+	if target%nums[l] == 0 {
+		return isValidEquation(target/nums[l], nums[:l])
+	}
+	return false
+}
+
+func sumEquations(validTotals []int) int {
+	total := 0
+	for _, val := range validTotals {
+		total += val
+	}
+	return total
+}
